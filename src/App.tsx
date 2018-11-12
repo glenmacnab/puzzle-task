3687aee85aa8db57dbb4f651463f1f655cfcd044
glen macnab <glenmacnab@gmail.com>
2018-11-12 13:42:36 +1100

    Clean up.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import './App.css';
 import Puzzle from './puzzle/Puzzle';
 
 
--- src/puzzle/Puzzle.tsx
+++ src/puzzle/Puzzle.tsx
@@ -9,6 +9,7 @@ const GameBoard = styled('div')`
     padding: 10px;
     border: solid 2px #333;
     border-radius: 4px;
+    margin: 50px auto;
 `
 
 const StyledButton = styled('button')`
@@ -21,15 +22,25 @@ const StyledButton = styled('button')`
     border-radius: 4px;
 `
 
+const StyledComplete = styled('div')`
+    position: absolute;
+    background: rgba(0,0,0,0.7);
+    line-height: 90px;
+    font-size: 50px;
+    color: #fff;
+`
+
 
 export default class Puzzle extends React.Component {
 
     numTiles: number = 16;
     lines: number = Math.sqrt(this.numTiles);
+    finishedString: string = "";
 
     state = {
         currentPos: this.numTiles - 1,
-        tiles: this.getTiles()
+        tiles: this.getTiles(),
+        complete: false
     }
 
     constructor(props: any) {
@@ -40,21 +51,26 @@ export default class Puzzle extends React.Component {
         this.initBoard(this.numTiles);
     }
 
-
     initBoard(numTiles: number) {
         this.numTiles = numTiles;
         this.lines = Math.sqrt(this.numTiles);
+        let newTiles = this.getTiles();
 
         this.state = {
             currentPos: this.numTiles - 1,
-            tiles: this.getTiles()
+            tiles: newTiles,
+            complete: false
         }
+
+        this.finishedString = newTiles.join();
 
         this.messItUp();
     }
 
-    getTiles() {
-        // Create tiles and put the blank one (tile 0) at the end of the array.
+    /**
+    * @returns Create tiles and put the blank one (tile 0) at the end of the array.
+    */
+    getTiles(): Array<number> {
         let tiles = [];
         for (let i = 1; i < this.numTiles; i++) {
             tiles.push(i);
@@ -63,11 +79,9 @@ export default class Puzzle extends React.Component {
         return tiles;
     }
 
-
-
     messItUp() {
         // bigger board needs more messing 
-        const moves = 200;
+        const moves = this.numTiles * 30;
         let i = 0;
         let tempPos = this.state.currentPos;
         let tiles = this.state.tiles;
@@ -86,9 +100,14 @@ export default class Puzzle extends React.Component {
 
         this.setState({ tiles: tiles })
         this.setState({ currentPos: tempPos })
-
+        this.setState({ complete: false })
     }
 
+
+    /**
+    * @param pos  A position in the array.
+    * @returns An array of possible positions to move to.
+    */
     possibleMoves(pos: number): Array<number> {
         let pmoves: Array<number> = [];
         for (let i = 0; i < this.numTiles; i++) {
@@ -98,6 +117,13 @@ export default class Puzzle extends React.Component {
         }
         return pmoves;
     }
+
+
+    /**
+     * @param pos1  A position in the array
+     * @param pos2  A position in the array
+     * @return boolean If pos1 and pos2 are 1 position away on the grid.
+     */
 
     isAdjacent(pos1: number, pos2: number): boolean {
         const pos1X = Math.abs(pos1 % this.lines) + 1;
@@ -118,10 +144,18 @@ export default class Puzzle extends React.Component {
     onClicked(pos: number) {
         if (this.isAdjacent(this.state.currentPos, pos)) {
             this.swapTiles(this.state.currentPos, pos);
+            if (this.state.tiles.join() == this.finishedString) {
+                this.setState({ complete: true })
+            }
         }
     }
 
-    swapTiles(pos1: number, pos2: number): any {
+    /**
+     * Swaps the position of two tiles in the array and sets the new state.
+     * @param pos1  A position in the array
+     * @param pos2  A position in the array
+     */
+    swapTiles(pos1: number, pos2: number) {
         const tiles = this.state.tiles;
         var temp = tiles[pos1];
         tiles[pos1] = tiles[pos2];
@@ -130,7 +164,6 @@ export default class Puzzle extends React.Component {
         this.setState({ currentPos: pos2 })
     }
 
-    
 
     render() {
         return (
@@ -148,16 +181,23 @@ export default class Puzzle extends React.Component {
                         )
                     })
                     }
-                </GameBoard>
 
+                    {this.state.complete == true && (
+                        <StyledComplete style={{ width: this.lines * 100, height: this.lines * 100 }}>
+                            YOU DID IT!
+                        </StyledComplete>
+                    )
+                    }
+
+
+
+                </GameBoard>
                 <div>
                     <StyledButton onClick={(e) => this.initBoard(9)}>3x3</StyledButton>
                     <StyledButton onClick={(e) => this.initBoard(16)}> 4x4</StyledButton>
                     <StyledButton onClick={(e) => this.initBoard(25)}> 5x5</StyledButton>
                     <StyledButton onClick={(e) => this.initBoard(36)}> 6x6</StyledButton>
                 </div>
-
-
             </div>
 
 
